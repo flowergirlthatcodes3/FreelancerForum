38633f78d15122833eda0d0a886cbcefb741d2dd
flowergirlthatcodes3 <149510282+flowergirlthatcodes3@users.noreply.github.com>
2023-11-29 04:58:02 -0500

    updating

--- freelancer.js
+++ freelancer.js
@@ -15,12 +15,12 @@ const occupations = [
     "Weather Agent"
 ]
 const prices = [
-"$650",
-"$450",
-"$375",
-"$750",
-"$800",
-"$950"
+"650",
+"450",
+"375",
+"750",
+"800",
+"950"
 ]
 //create a function that will generate random freelancers
 function rndmFreelancer(){
@@ -40,44 +40,66 @@ const rndmfreelancer2 = rndmFreelancer()
 const rndmfreelancer3 = rndmFreelancer()
 const rndmfreelancer4 = rndmFreelancer()
 const freelancers = [rndmfreelancer1, rndmfreelancer2, rndmfreelancer3, rndmfreelancer4]
-const ul = document.querySelector(".list")
+const freelancersContainer = document.querySelector("#freelancers")
+const priceContainer = document.querySelector("#AvestrtPrice")
+//const h2 = document.querySelector("h2")
 //const ul = document.querySelector(".list")
 // create a function to render freelance data
 
-const interval = setInterval(() => {
+interval = setInterval(() => {
     const rnd = rndmFreelancer()
     freelancers.push(rnd)
     render()
-    if(freelancers.length === 30){
+    if(freelancers.length === 40){
         clearInterval(interval)
     }
+}, 2000)
 
-}, 3000)
 function render() {
     const html = freelancers.map((freelancers) => {
-        return `<li> Name: </br>${freelancers.Name} </br>
+        return `<span> Name: </br>${freelancers.Name} </br>
         Occupation: </br> ${freelancers.Occupation} </br>
-        Price: </br> ${freelancers.Price}</li>`
+        Price: </br> ${freelancers.Price} <hr/></span>`
         //return `<li>Name: ${freelancers.Name} Occupation: ${freelancers.Occupation} Price: ${freelancers.Price}</li>`
     })
-
-    ul.innerHTML = html.join("")
+  freelancersContainer.innerHTML = html.join("")
 //ul.innerHTML = html.join("")
 console.log(html.join(""))
+   
 }
-const h2 = document.querySelector("h1")
-setInterval(() => {
-    h2.innerHTML = h2.innerHTML
-} , 2000)
+    const arr = [650,450,375,750,800,950]  
+let average = arr.reduce((a,b) => a + b, 0) / arr.length
+console.log(average)
+priceContainer.replaceChildren(`The Average Freelancer Cost Is $${average}` , setInterval(average, 2000))
+// tried to make the average update but didnt quite know how.    
+    
+    
+    
+    
+    
+   // -----> trying/practicing different options. 
+    //let sum = 0
+    //for (let i = 0; i < freelancers.length; i++) {
+       // sum += freelancers[i]; 
+        //let totalPrices = prices.reduce((sum, prices) => 
+        //sum + freelancers.Price, 0)
+      //const totalPrices = prices.reduce((sum, prices) =>
+        //sum + freelancers.Price, 0)
+    //const averagePrice = totalPrices / freelancers.length
+    //return averagePrice
+//let averagePrice = (sum / prices.length)
+    //priceContainer.innerHTML = html.join("")
+  
+    /*const averagePriceSpan = document.querySelector("#averagePrice")
+    averagePriceSpan.innerHTML = `${averagePrice.toFixed(2)}`
+    return sum / prices.length*/
+  
+//const totalPrices = prices.reduce((sum, price) => sum + freelancers.Price, 0)
+//const averagePrice = totalPrices / freelancers.length
 
-
-function getAverage(prices) {
-    //const total = prices.reduce((acc,curr) => acc + curr, 0)
-    //return total / prices.length
-
-}
+//return sum / prices.length
 //const ave = getAverage(prices);
-c//onsole.log(getAverage(prices))
+//onsole.log(getAverage(prices))
    /*prices.forEach(function(item,index) {
 total += item;
 count++
@@ -85,4 +107,3 @@ count++
 return total/count
 }
 console.log(getAverage(total/count))*/
-    
